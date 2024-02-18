b3d0c1cf05faeb49fa082aa10abd8b59bcc391a9
GhanIs05 <154982500+GhanIs05@users.noreply.github.com>
2024-02-18 21:53:44 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -15,7 +15,9 @@ public class MainActivity extends AppCompatActivity {
     RadioButton b2;
     Button ok;
     TextView result;
-    float temperature;
+    Double temperature;
+    String str;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,23 +25,33 @@ public class MainActivity extends AppCompatActivity {
         temp = (EditText) findViewById(R.id._e_temp);
          b2 = findViewById(R.id._r_farenheit);
          b1 = findViewById(R.id._r_celcius);
-         ok = findViewById(R.id._b_ok);
+         ok = (Button) findViewById(R.id._b_ok);
          result = findViewById(R.id._t_result);
+         temperature = 0.0;
+         str = "0.0";
     }
 
     public void convertTemperature(View view) {
-        String str = result.getText().toString();
-        result.setText("0");
+        str = result.getText().toString();
+        result.setText("Enter value");
         if (b1.isChecked()) {
             str = temp.getText().toString();
-            temperature = Float.parseFloat(str);
-            float celcius = (5.0f / 9.0f) * (temperature - 32);
-            result.setText("Result" + celcius+"c");
+            try {
+                temperature = Double.parseDouble(str);
+                double celcius = (5.0 / 9.0) * (temperature - 32);
+                result.setText("Result" + celcius + "c");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } else if (b2.isChecked()) {
             str = temp.getText().toString();
-            temperature = Float.parseFloat(str);
-            float farenheit = temperature * (9.0f / 5.0f) + 32;
-            result.setText("Result: "+farenheit+"f");
+            try {
+                temperature = Double.parseDouble(str);
+                double farenheit = temperature * (9.0 / 5.0) + 32;
+                result.setText("Result: " + farenheit + "f");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
